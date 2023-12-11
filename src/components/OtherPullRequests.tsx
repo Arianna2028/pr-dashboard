6b47e3294b6b675807be9c9de47153f7d24cf5ac
Ari <Arianna2028@users.noreply.github.com>
2023-12-11 11:07:51 -0500

    feat: add draft toggle

--- src/components/OtherPullRequests.tsx
+++ src/components/OtherPullRequests.tsx
@@ -4,28 +4,51 @@ import { PullRequestIconColumn } from "./PullRequestIconColumn";
 import { BsCheckCircle } from "react-icons/bs";
 import { BiSolidUser } from "react-icons/bi";
 import { PullRequestObject } from "../backend/models/PullRequestObject";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from 'mobx-react';
 import PullRequestStore from "../store/PullRequestStore";
-
+import Form from 'react-bootstrap/Form';
 
 
 export const OtherPullRequests = observer(() => {
-  var pullRequestRows: JSX.Element[] = [];
-  for (let i = 0; i < PullRequestStore.otherPullRequests.length; i++) {
-    pullRequestRows.push(
-      <PullRequestRow
-        key={PullRequestStore.otherPullRequests[i].id}
-        pr={PullRequestStore.otherPullRequests[i]}
-        reviewStatus={PullRequestStore.otherPullRequests[i].myApprovalStatus || "NOT_YET_REVIEWED"}
-        accountForComments={true}
-      />
-    );
-  }
+  const [showDrafts, setShowDrafts] = useState(true);
+  const [pullRequestRowsDisplay, setPullRequestRowsDisplay] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    var pullRequestRows: JSX.Element[] = [];
+    for (let i = 0; i < PullRequestStore.otherPullRequests.length; i++) {
+      if (showDrafts || !PullRequestStore.otherPullRequests[i].draft) {
+        pullRequestRows.push(
+          <PullRequestRow
+            key={PullRequestStore.otherPullRequests[i].id}
+            pr={PullRequestStore.otherPullRequests[i]}
+            reviewStatus={PullRequestStore.otherPullRequests[i].myApprovalStatus || "NOT_YET_REVIEWED"}
+            accountForComments={true}
+          />
+        );
+      }
+    }
+    setPullRequestRowsDisplay(pullRequestRows);
+  }, [showDrafts])
 
   return (
     <Card className="pr-card mt-3">
-      <Card.Title className={"px-3 py-2 mb-0 text-white"}>Open PRs</Card.Title>
+      <Card.Title className={"px-3 py-2 mb-0 text-white"}>
+        <Row>
+          <Col>
+            <h4 className="mb-0">Open PRs</h4>
+          </Col>
+          <Col className="col-auto">
+            <Form.Check
+              type="switch"
+              id="open-prs-draft-switch"
+              label="Show Drafts"
+              checked={showDrafts}
+              onChange={(e) => setShowDrafts(e.target.checked)}
+            />
+          </Col>
+        </Row>
+      </Card.Title>
       <Card.Body>
         <div className={"pr-table text-white mx-3"}>
           <Row className={"py-2"}>
@@ -39,7 +62,7 @@ export const OtherPullRequests = observer(() => {
             <Col>{/* We don't want to label the "label" column, but need to keep the layout. */}</Col>
           </Row>
 
-          {pullRequestRows}
+          {pullRequestRowsDisplay}
         </div>
       </Card.Body>
     </Card>
